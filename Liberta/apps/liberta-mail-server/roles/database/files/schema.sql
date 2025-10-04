d41f68ca4ec9e3ca2ab90016d503c5c7d0bb386e
appzer0 <51806-appzer0@users.noreply.framagit.org>
2025-10-04 18:07:20 +0200

    Increase VARCHAR for various fields in schema.sql

--- Liberta/apps/liberta-mail-server/roles/database/files/schema.sql
+++ Liberta/apps/liberta-mail-server/roles/database/files/schema.sql
@@ -8,7 +8,7 @@ DROP TABLE IF EXISTS `domain`;
 
 CREATE TABLE `domain` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `domain` varchar(50) NOT NULL,
+  `domain` varchar(64) NOT NULL,
   `transport` varchar(200) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
@@ -20,8 +20,8 @@ CREATE TABLE `domain` (
 CREATE TABLE `alias` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `domain_id` int(11) NOT NULL,
-  `source` varchar(100) NOT NULL,
-  `destination` varchar(100) NOT NULL,
+  `source` varchar(128) NOT NULL,
+  `destination` varchar(128) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `source` (`source`,`destination`),
   KEY `domain_id` (`domain_id`),
@@ -36,9 +36,9 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `domain_id` int(11) NOT NULL,
   `email` varchar(128) NOT NULL DEFAULT '',
-  `crypt` varchar(256) NOT NULL DEFAULT '',
+  `crypt` varchar(767) NOT NULL DEFAULT '',
   `quota` tinytext NOT NULL,
-  `note` varchar(128) NOT NULL DEFAULT '',
+  `note` varchar(255) NOT NULL DEFAULT '',
   `active` tinyint(4) NOT NULL DEFAULT 1,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
